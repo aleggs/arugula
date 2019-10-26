e243c9d737b328647b94b8d4f65a8b4aa6b9073a
Alex Xu <aleggsxu@gmail.com>
2019-10-25 21:00:58 -0700

    Initialized xcode proj, firebase in progress

---
+++ Arugula/Pods/Target Support Files/Pods-Arugula-ArugulaUITests/Pods-Arugula-ArugulaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Arugula_ArugulaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Arugula_ArugulaUITests
+@end
---
+++ Arugula/Pods/Target Support Files/Pods-ArugulaTests/Pods-ArugulaTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ArugulaTests : NSObject
+@end
+@implementation PodsDummy_Pods_ArugulaTests
+@end
