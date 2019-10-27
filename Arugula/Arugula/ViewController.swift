5035a382a47983d2ece0460a1a1a820200da7a88
Alex Xu <aleggsxu@gmail.com>
2019-10-27 02:53:11 -0700

    hol up

--- Arugula/Arugula/ViewController.swift
+++ Arugula/Arugula/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 import FirebaseUI
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, SwipeableCardViewDataSource {
+    
+    
+    @IBOutlet private weak var swipeableCardView: SwipeableCardViewContainer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        swipeableCardView?.dataSource = self
+
     }
+    
     @IBAction func LoginPressed(_ sender: Any) {
         
         let authUI = FUIAuth.defaultAuthUI()
@@ -22,11 +28,9 @@ class ViewController: UIViewController {
             return
         }
         authUI?.delegate = self
-        
 
         let providers: [FUIAuthProvider] = [
           FUIGoogleAuth(),
-//          FUIEmailAuth.initAuthAuthUI(FUIAuth.defaultAuthUI(), signInMethod: FIREmailLinkAuthSignInMethod, forceSameDevice: false, allowNewEmailAccounts: true, actionCodeSetting: actionCodeSettings)
         ]
         authUI?.providers = providers
         
@@ -39,12 +43,12 @@ class ViewController: UIViewController {
           // other URL handling goes here.
           return false
         }
-
         
         let authViewController = authUI!.authViewController()
         present(authViewController, animated: true, completion: nil)
-        
     }
+ 
+    
     
 }
 
@@ -59,3 +63,64 @@ extension ViewController: FUIAuthDelegate{
         performSegue(withIdentifier: "toWelcome", sender: self)
     }
 }
+
+extension ViewController {
+
+    func numberOfCards() -> Int {
+        return viewModels.count
+    }
+
+    func card(forItemAtIndex index: Int) -> SwipeableCardViewCard {
+        let viewModel = viewModels[index]
+        let cardView = SampleSwipeableCard()
+        cardView.viewModel = viewModel
+        return cardView
+    }
+
+    func viewForEmptyCards() -> UIView? {
+        return nil
+    }
+
+}
+
+
+extension ViewController {
+
+    var viewModels: [SampleSwipeableCellViewModel] {
+
+        let hamburger = SampleSwipeableCellViewModel(title: "McDonalds",
+                                                     subtitle: "Hamburger",
+                                                     color: UIColor(red:0.96, green:0.81, blue:0.46, alpha:1.0),
+                                                     image: #imageLiteral(resourceName: "hamburger"))
+
+        let panda = SampleSwipeableCellViewModel(title: "Panda",
+                                                  subtitle: "Animal",
+                                                  color: UIColor(red:0.29, green:0.64, blue:0.96, alpha:1.0),
+                                                  image: #imageLiteral(resourceName: "panda"))
+
+        let puppy = SampleSwipeableCellViewModel(title: "Puppy",
+                                                  subtitle: "Pet",
+                                                  color: UIColor(red:0.29, green:0.63, blue:0.49, alpha:1.0),
+                                                  image: #imageLiteral(resourceName: "puppy"))
+
+        let poop = SampleSwipeableCellViewModel(title: "Poop",
+                                                  subtitle: "Smelly",
+                                                  color: UIColor(red:0.69, green:0.52, blue:0.38, alpha:1.0),
+                                                  image: #imageLiteral(resourceName: "poop"))
+
+        let robot = SampleSwipeableCellViewModel(title: "Robot",
+                                                  subtitle: "Future",
+                                                  color: UIColor(red:0.90, green:0.99, blue:0.97, alpha:1.0),
+                                                  image: #imageLiteral(resourceName: "robot"))
+
+        let clown = SampleSwipeableCellViewModel(title: "Clown",
+                                                  subtitle: "Scary",
+                                                  color: UIColor(red:0.83, green:0.82, blue:0.69, alpha:1.0),
+                                                  image: #imageLiteral(resourceName: "clown"))
+
+        return [hamburger, panda, puppy, poop, robot, clown]
+    }
+
+}
+
+
